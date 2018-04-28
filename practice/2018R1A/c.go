c34b28895101322f39e4f758ab67371602d49897
Gildas Chabot <gildas.chabot@schibsted.com>
2018-04-28 17:59:04 +0200

    2018R1A-c: fix bugs

--- practice/2018R1A/c.go
+++ practice/2018R1A/c.go
@@ -124,15 +124,22 @@ func (i *Intervals) AddToAll(l, h float64) {
 }
 
 func (i *Intervals) Len() int           { return len(i.a) }
-func (i *Intervals) Swap(a, b int)      { i.a[a], i.a[b] = i.a[a], i.a[b] }
+func (i *Intervals) Swap(a, b int)      { i.a[a], i.a[b] = i.a[b], i.a[a] }
 func (i *Intervals) Less(a, b int) bool { return i.a[a].L < i.a[b].L }
+
+func max(a, b float64) float64 {
+	if a > b {
+		return a
+	}
+	return b
+}
 
 func (i *Intervals) SortAndMerge() {
 	sort.Sort(i)
 
 	for k := 0; k < len(i.a)-1; k++ {
 		if i.a[k].H >= i.a[k+1].L {
-			i.a[k].H = i.a[k+1].H
+			i.a[k].H = max(i.a[k].H, i.a[k+1].H)
 			i.a = append(i.a[:k+1], i.a[k+2:]...)
 			k--
 		}
